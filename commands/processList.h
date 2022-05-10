31bacdfe882b3de66568310b7dfabb430ab00980
bieenr <bien08060@google.com>
2022-05-10 08:46:15 +0700

    linh tinh

--- commands/processList.h
+++ commands/processList.h
@@ -6,4 +6,8 @@ const int maxprocess = 3;
 
 STARTUPINFO si[maxprocess];
 PROCESS_INFORMATION pi[maxprocess];
+
 int num_process = 0;
+
+int mode = 0 ;    // mode = 0 <=> mode = foregroundMode ; mode = 1 <=> mode = backgroundMode
+bool pi_run[maxprocess];
